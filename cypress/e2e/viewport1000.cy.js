4115cb74cb096d3d289d243ede576e078545d017
Di <hanwi1994@gmail.com>
2023-06-30 19:01:19 +0200

    add tests for Age In Days page

--- cypress/e2e/viewport1000.cy.js
+++ cypress/e2e/viewport1000.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress"/>
 describe("Main page tests", () => {
   beforeEach(function () {
     cy.visit("/");
@@ -34,5 +35,48 @@ describe("Main page tests", () => {
     cy.get('[data-cy="github-badge"]').should("be.visible");
     cy.get('[data-cy="instagram-badge"]').should("be.visible");
     cy.get(".logo").should("be.hidden");
+  });
+
+  it("visits Age in Days Page", () => {
+    cy.get('[data-cy="project-two"]').find(".project-link").click();
+    cy.url().should("be.equal", "https://dileit.com/age-in-days.html");
+  });
+});
+
+describe("Age in Days Page Tests", () => {
+  beforeEach(function () {
+    cy.visit("/age-in-days.html");
+  });
+
+  it("visits About me section via the navigation bar", () => {
+    cy.get('[data-cy="catpaw-button"]').click();
+    cy.get('[data-cy="about-link"]').click();
+    cy.url().should("be.equal", "https://dileit.com/index.html#about-me");
+    cy.get("#about-me").should("contain", "Hello. My name is Diana Voronova");
+  });
+
+  it("visits Projects section via the navigation bar", () => {
+    cy.get('[data-cy="catpaw-button"]').click();
+    cy.get('[data-cy="projects-link"]').click();
+    cy.url().should("be.equal", "https://dileit.com/index.html#projects");
+    cy.get('[data-cy="project-container"]').should("contain", "UITAP", "Age In Days Calculator");
+    cy.get('[data-cy="project-one"]').should("be.visible");
+    cy.get('[data-cy="project-two"]').should("be.visible");
+    cy.get('[data-cy="project-three"]').should("be.visible");
+    // cy.get('[data-cy="project-four"]').should("be.visible");
+  });
+
+  it("goes to Contact Me section via the navigation bar", () => {
+    cy.get('[data-cy="catpaw-button"]').click();
+    cy.get('[data-cy="contactme-link"]').click();
+    cy.url().should("be.equal", "https://dileit.com/age-in-days.html#contact-me");
+    cy.get('[data-cy="contactme"]').should("contain", "hanwi1994@gmail.com", "LinkedIn");
+  });
+
+  it("checks work of calculator", () => {
+    cy.get('[data-test="date-input"]').type("1994-10-10").trigger("change");
+    cy.get('[data-test="result"]')
+      .invoke("text")
+      .should("match", /\d{1,5}/);
   });
 });
