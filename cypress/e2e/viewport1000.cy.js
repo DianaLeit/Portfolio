e80825d6c162f8f9a8068985fd7ac0e3241ef48b
Leit <hanwi1994@gmail.com>
2023-10-09 18:30:33 +0200

    update cypress tests for 1000

--- cypress/e2e/viewport1000.cy.js
+++ cypress/e2e/viewport1000.cy.js
@@ -3,77 +3,16 @@ describe("Main page tests", () => {
   beforeEach(function () {
     cy.visit("/");
   });
-  it("goes to About me section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="about-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/#about-me");
-    cy.get("#about-me").should("contain", "Hello. My name is Diana Voronova");
-  });
-  it("goes to About me section via the button", () => {
-    cy.get('[data-cy="about-button"]').click();
-    cy.url().should("be.equal", "https://dileit.com/#about-me");
-    cy.get("#about-me").should("contain", "Hello. My name is Diana Voronova");
-  });
-  it("goes to Projects section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="projects-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/#projects");
-    cy.get('[data-cy="project-container"]').should("contain", "UITAP", "Age In Days Calculator");
-    cy.get('[data-cy="project-one"]').should("be.visible");
-    cy.get('[data-cy="project-two"]').should("be.visible");
-    cy.get('[data-cy="project-three"]').should("be.visible");
-    // cy.get('[data-cy="project-four"]').should("be.visible");
-  });
-  it("goes to Contact Me section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="contactme-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/#contact-me");
-    cy.get('[data-cy="contactme"]').should("contain", "hanwi1994@gmail.com", "LinkedIn");
-  });
-  it("checks socials badges and logo", () => {
-    cy.get('[data-cy="linkedin-badge"]').should("be.visible");
-    cy.get('[data-cy="github-badge"]').should("be.visible");
-    cy.get('[data-cy="instagram-badge"]').should("be.visible");
-    cy.get(".logo").should("be.hidden");
-  });
-
-  it("visits Age in Days Page", () => {
-    cy.get('[data-cy="project-two"]').find(".project-link").click();
-    cy.url().should("be.equal", "https://dileit.com/age-in-days.html");
-  });
-});
-
-describe("Age in Days Page Tests", () => {
-  beforeEach(function () {
-    cy.visit("/age-in-days.html");
-  });
-
-  it("visits About me section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="about-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/index.html#about-me");
-    cy.get("#about-me").should("contain", "Hello. My name is Diana Voronova");
-  });
-
-  it("visits Projects section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="projects-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/index.html#projects");
-    cy.get('[data-cy="project-container"]').should("contain", "UITAP", "Age In Days Calculator");
-    cy.get('[data-cy="project-one"]').should("be.visible");
-    cy.get('[data-cy="project-two"]').should("be.visible");
-    cy.get('[data-cy="project-three"]').should("be.visible");
-    // cy.get('[data-cy="project-four"]').should("be.visible");
-  });
-
-  it("goes to Contact Me section via the navigation bar", () => {
-    cy.get('[data-cy="catpaw-button"]').click();
-    cy.get('[data-cy="contactme-link"]').click();
-    cy.url().should("be.equal", "https://dileit.com/age-in-days.html#contact-me");
-    cy.get('[data-cy="contactme"]').should("contain", "hanwi1994@gmail.com", "LinkedIn");
+  it("checks links buttons", () => {
+    cy.get('[data-cy="mail"]').should("be.visible");
+    cy.get('[data-cy="cv"]').should("be.visible");
+    cy.get('[data-cy="linkedin"]').should("be.visible");
+    cy.get('[data-cy="github"]').should("be.visible");
+    cy.get('[data-cy="insta"]').should("be.visible");
   });
 
   it("checks work of calculator", () => {
+    cy.get('[data-cy="project-2"]').find('[data-cy="My-Age-in-Days-button"]').click();
     cy.get('[data-test="date-input"]').type("1994-10-10").trigger("change");
     cy.get('[data-test="result"]')
       .invoke("text")
